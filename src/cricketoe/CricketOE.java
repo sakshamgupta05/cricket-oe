9f459f521d4b2d1c3527acdbbe0bb450fcf5e2ef
Saksham Gupta <sakshamgupta05@gmail.com>
2018-11-30 18:54:31 +0530

    proj setup

---
+++ src/cricketoe/CricketOE.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cricketoe;
+
+/**
+ *
+ * @author Saksham
+ */
+public class CricketOE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NewJFrame cal=new NewJFrame();
+        cal.setVisible(true);
+    }
+    
+}
